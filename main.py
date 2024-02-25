36bd051c3bfa5b8b08963e9a3ca3d2b219a25b15
fly-toribird <96984915+fly-toribird@users.noreply.github.com>
2024-02-25 13:18:29 +0900

    formula classとmathanimal classを作った

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+class formula:
+    def __init__(self,formula):
+        self.formula = formula
+
+
+def gen_formula(formula):
+    return formula(formula)
+
 class Math_animal:
-    def __init__(self,unique_name,):
+    def __init__(self,unique_number,gene,input,output):
+        self.unique_number = unique_number
+        self.gene:int = gene
+        self.input:int = input
+        self.output:int = output
